e6594184f27b2766775eb6520a3fc1c05b35c81a
gventurini <intesasanpaolo>
2020-12-23 22:52:14 +0100

    add #addPath() unit tests

--- test/asd.spec.ts
+++ test/asd.spec.ts
@@ -7,6 +7,7 @@ import { UrlBuilder } from '../src/index';
 describe('UrlBuilder', () => {
 
   const exampleUrl = 'https://localhost:8080';
+  const examplePath = 'test';
 
   describe('#create()', () => {
     it(`should return ${exampleUrl} input values are 'localhost', '8080'`, () => {
@@ -21,6 +22,42 @@ describe('UrlBuilder', () => {
       const url = UrlBuilder.create('localhost', 8080, false).build();
 
       expect(url).to.equal(httpExampleUrl);
+    });
+  });
+
+  describe('#addPath()', () => {
+
+    const paths = [`${examplePath}`, `/${examplePath}`, `${examplePath}/`, `/${examplePath}/`];
+
+    paths.forEach((path) => {
+      it(`should return {basePath}/${examplePath} when its value is '${path}'`, () => {
+        const url = UrlBuilder.create('localhost', 8080)
+          .addPath(path)
+          .build();
+
+        expect(url).to.equal(`${exampleUrl}/${examplePath}`);
+      });
+    });
+
+    paths.forEach((path) => {
+      it(`should return {basePath}/${examplePath}/ when its values are '${path}', 'true'`, () => {
+        const url = UrlBuilder.create('localhost', 8080)
+          .addPath(path, true)
+          .build();
+
+        expect(url).to.equal(`${exampleUrl}/${examplePath}/`);
+      });
+    });
+
+    it('should not modify the original path', () => {
+      const url = UrlBuilder.create('localhost', 8080);
+
+      expect(url.build()).to.equal(exampleUrl);
+
+      const newUrl = url.addPath(examplePath);
+
+      expect(url.build()).to.equal(exampleUrl);
+      expect(newUrl.build()).to.equal(`${exampleUrl}/${examplePath}`);
     });
   });
 });
