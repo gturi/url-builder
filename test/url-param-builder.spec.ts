c729934f676418cb77cc9c09115f92ead7a01adc
gventurini <intesasanpaolo>
2020-12-26 17:06:38 +0100

    add tests with empty strings keys

--- test/url-param-builder.spec.ts
+++ test/url-param-builder.spec.ts
@@ -6,12 +6,14 @@ import { UrlBuilder } from '../src/index';
 
 describe('UrlParamBuilder', () => {
 
-  const exampleUrl = 'https://localhost:8080';
+  const host = 'localhost';
+  const port = 8080;
+  const exampleUrl = `https://${host}:${port}`;
   const examplePath = 'test';
 
   describe('#addQueryParam()', () => {
     it('should insert \'?\' to separate the first param from the path', () => {
-      const url = UrlBuilder.create('localhost', 8080)
+      const url = UrlBuilder.create(host, port)
         .addPath(examplePath)
         .addQueryParam('foo', 'bar')
         .build();
@@ -20,7 +22,7 @@ describe('UrlParamBuilder', () => {
     });
 
     it('should insert \'&\' to separate query params from the second param onwards', () => {
-      const url = UrlBuilder.create('localhost', 8080)
+      const url = UrlBuilder.create(host, port)
         .addPath(examplePath)
         .addQueryParam('foo', 'bar')
         .addQueryParam('baz', 'qux')
@@ -31,7 +33,7 @@ describe('UrlParamBuilder', () => {
     });
 
     it('should allow numeric query params', () => {
-      const url = UrlBuilder.create('localhost', 8080)
+      const url = UrlBuilder.create(host, port)
         .addPath(examplePath)
         .addQueryParam('foo', 1)
         .addQueryParam('bar', 2)
@@ -41,7 +43,7 @@ describe('UrlParamBuilder', () => {
     });
 
     it('should allow boolean query params', () => {
-      const url = UrlBuilder.create('localhost', 8080)
+      const url = UrlBuilder.create(host, port)
         .addPath(examplePath)
         .addQueryParam('foo', true)
         .addQueryParam('bar', false)
@@ -56,12 +58,33 @@ describe('UrlParamBuilder', () => {
         baz: 0,
         qux: true,
       };
-      const url = UrlBuilder.create('localhost', 8080)
+      const url = UrlBuilder.create(host, port)
         .addPath(examplePath)
         .addQueryParam('obj', obj)
         .build();
 
       expect(url).to.equal(`${exampleUrl}/${examplePath}?obj={"foo":"bar","baz":0,"qux":true}`);
+    });
+
+    it('should return the same UrlParamBuilder reference if the key is empty string', () => {
+      const urlBuilder = UrlBuilder.create(host, port)
+        .addPath(examplePath);
+
+      const urlBuilder2 = urlBuilder.addQueryParam('', 'foo');
+      expect(urlBuilder === urlBuilder2).to.equal(true);
+      expect(urlBuilder).to.equal(urlBuilder2);
+
+      expect(urlBuilder.build()).to.equal(`${exampleUrl}/${examplePath}`);
+    });
+
+    it('should separate the query param from the base url with \'?\' until the input key is equal to empty string', () => {
+      const urlBuilder = UrlBuilder.create(host, port)
+        .addPath(examplePath)
+        .addQueryParam('', 'foo')
+        .addQueryParam('', 'bar')
+        .addQueryParam('baz', 'qux');
+
+      expect(urlBuilder.build()).to.equal(`${exampleUrl}/${examplePath}?baz=qux`);
     });
   });
 });
