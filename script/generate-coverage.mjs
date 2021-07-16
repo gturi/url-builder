abb1a8a8bcb6410b8c69f3ada56224d188c9e379
gventurini <giacomo.venturini3@gmail.com>
2021-07-16 22:15:33 +0200

    - create script to generate typedoc and coverage - add gh pages workflow

--- script/generate-coverage.mjs
+++ script/generate-coverage.mjs
@@ -6,9 +6,6 @@ const arg = process.argv[2] || 'unknown';
 const deployDir = `gh-pages/${arg}`;
 const options = { stdio: [0, 1, 2] };
 
-// generate typedoc
-execSync(`npx typedoc --out ${deployDir}/docs/ src/`, options);
-
 // generate coverage
 const coverageDir = `${deployDir}/coverage`;
 execSync(`nyc --report-dir=${coverageDir} -r lcov -e .ts -x "*.spec.ts" npm run test`, options);
---
+++ script/generate-typedoc.mjs
@@ -0,0 +1,10 @@
+import { execSync } from 'child_process';
+
+// Default value `unknown` if no args provided via CLI.
+const arg = process.argv[2] || 'unknown';
+
+const deployDir = `gh-pages/${arg}`;
+const options = { stdio: [0, 1, 2] };
+
+// generate typedoc
+execSync(`npx typedoc --out ${deployDir}/docs/ src/`, options);
