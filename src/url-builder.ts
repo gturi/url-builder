46b38908717cd0d4f42815f7af5b52493cf9d260
gventurini <giacomo.venturini3@gmail.com>
2021-07-19 21:52:30 +0200

    add path variable method

--- src/url-builder.ts
+++ src/url-builder.ts
@@ -35,18 +35,49 @@ export class UrlBuilder extends UrlParamBuilder {
    * @returns a new {@link UrlBuilder} instance.
    */
   addPath(path: string, trailingSeparator = false): UrlBuilder {
-    const trimmedPath = path.trim();
-    if (path === '') {
+    let trimmedPath = path.trim();
+    if (trimmedPath === '') {
       return this;
     }
     UrlBuilder.failIfStringHasWhitespaces(trimmedPath);
-    const newUrl = [
-      // removes all the trailing '/'
-      this.url.replace(/[/]*$/g, ''),
-      // removes all the leading and trailing '/'
-      trimmedPath.replace(/(^[/]*|[/]*$)/g, ''),
-    ].join('/'); // the result is 'newUrl[0]/newUrl[1]'
-    return new UrlBuilder(newUrl + (trailingSeparator ? '/' : ''));
+    // removes all the leading and trailing '/'
+    trimmedPath = trimmedPath.replace(/(^[/]*|[/]*$)/g, '');
+    const newUrl = this.joinToCurrentUrl(trimmedPath);
+    return UrlBuilder.getNewUrlBuilder(newUrl, trailingSeparator);
+  }
+
+  /**
+   * Adds a path variable to the current url. The resulting url is obtained by appending
+   * to the base one '/' separator followed by the specified `path`. This method allows the va
+   *
+   * @param pathVariable the path that should be appended to the current url.
+   * @param trim whether the path variable should be trimmed.
+   * @param trailingSeparator if set to `true` adds a trailing '/' after the specified `path`.
+   *                          This property should be used only for compatibility reasons,
+   *                          since trailing '/' should be avoided when building rest APIs.
+   * @returns a new {@link UrlBuilder} instance.
+   */
+  addPathVariable(pathVariable: string, trim = false, trailingSeparator = false): UrlBuilder {
+    let newPathVariable = trim ? pathVariable.trim() : pathVariable;
+    if (newPathVariable === '') {
+      return this;
+    }
+    // replaces every '/' in the path variable with a '%2F'
+    newPathVariable = newPathVariable.replace(/[/]/g, '%2F');
+    const newUrl = this.joinToCurrentUrl(newPathVariable);
+    return UrlBuilder.getNewUrlBuilder(newUrl, trailingSeparator);
+  }
+
+  /**
+   * Joins a path to the current url using `/` as separator.
+   *
+   * @param path the path to join.
+   * @returns the concatenation of `this.url`/`path`.
+   */
+  private joinToCurrentUrl(path: string): string {
+    // removes all the trailing '/' from current url
+    const url = this.url.replace(/[/]*$/g, '');
+    return [url, path].join('/'); // the result is 'array[0]/array[1]'
   }
 
   /**
@@ -58,5 +89,16 @@ export class UrlBuilder extends UrlParamBuilder {
     if (/\s/.test(s)) {
       throw new Error('path can not contain any whitespace character');
     }
+  }
+
+  /**
+   * Gets a new {@link UrlBuilder} instance.
+   *
+   * @param newUrl the url used to initialize {@link UrlBuilder} constructor.
+   * @param trailingSeparator if set to `true` adds a trailing '/' after the specified `path`.
+   * @returns a new {@link UrlBuilder} instance.
+   */
+  private static getNewUrlBuilder(newUrl: string, trailingSeparator: boolean): UrlBuilder {
+    return new UrlBuilder(`${newUrl}${trailingSeparator ? '/' : ''}`);
   }
 }
